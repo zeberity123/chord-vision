6680c160826b31803c7fbecde65a8f43297d32ea
zeberity123 <darudayu123@gmail.com>
2024-05-29 11:54:27 +0900

    modified chord_run.py

--- Guitar_Chord_Recognition/chord_run.py
+++ Guitar_Chord_Recognition/chord_run.py
@@ -11,24 +11,30 @@ from torch import optim
 import torch.nn.functional as F
 from torchvision import datasets, transforms, models
 
-def which_chord():
-    # Display input controls 
-    print("Which chord will you capture data for?")
-    chord_option = widgets.Dropdown(
-    options=["C","D", "E","F","G","A","B", "_BG"],
-    value = "A",
-    description="Chord"
-    )
-
-    clean_current_chord_data = widgets.Checkbox(
-        value=False,
-        description="Clean existing training data? ",
-        disabled=False,
-        indent=False
-    )
-
-    display(chord_option)
-    display(clean_current_chord_data)
+from chord_utils import predict_image, get_random_images
 
 
-which_chord()
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+input_size = transforms.Compose([transforms.Resize(224),transforms.ToTensor(),])
+model=torch.load('chord_vision.pth')
+model.eval()
+
+
+chords = ['A', 'B', 'C', 'D', 'E', 'F', 'G','_BG']
+data_dir = 'output'
+
+to_pil = transforms.ToPILImage()
+images, labels = get_random_images(data_dir, input_size, 7)
+
+fig=plt.figure(figsize=(15,15))
+
+for i in range(len(images)):
+    image = to_pil(images[i])
+    index = predict_image(input_size, device, model, image)
+    sub = fig.add_subplot(1, len(images), i+1)
+    res = int(labels[i]) == index
+    sub.set_title(str(chords[index]) + ":" + str(res))
+    plt.axis('off')
+    plt.imshow(image)
+
+plt.show()
